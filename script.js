41506b71342a4919b58b475561d69677cd8dc419
avms <andrevmuzy@gmail.com>
2022-11-07 20:02:34 -0300

    canvas set

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-calc(){
-  glucose = document.getElementById('glucose')
-
-}
+date1 = document.getElementById('date1');
+date2 = document.getElementById('date2');
+date3 = document.getElementById('date3');
+date4 = document.getElementById('date4');
+date5 = document.getElementById('date5');
+date6 = document.getElementById('date6');
+date7 = document.getElementById('date7');
+glucose1 = document.getElementById('glucose1');
+glucose1 = document.getElementById('glucose2');
+glucose1 = document.getElementById('glucose3');
+glucose1 = document.getElementById('glucose4');
+glucose1 = document.getElementById('glucose5');
+glucose1 = document.getElementById('glucose6');
+glucose1 = document.getElementById('glucose7');
