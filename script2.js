c74b372e3b7fc5db5bd7a6ea994b9ee925cc75a6
avms <andrevmuzy@gmail.com>
2022-12-09 18:39:06 -0300

    calc function

--- script2.js
+++ script2.js
@@ -1,8 +1,13 @@
-function idade() {
+function calc() {
   var data = new Date();
   var ano = data.getFullYear();
   var fano = document.getElementById('dateofbirth');
   var idade = ano - Number(fano.value);
   var res = document.getElementById('age');
   res.innerHTML = ` ${idade}`;
+
+  var height = document.getElementById('height');
+  var weight = document.getElementById('weight');
+  var bmi = weight / (height**2)
+  
 }
